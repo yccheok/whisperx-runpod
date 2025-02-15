f4bbed9fe4ff55d378295d3a3f70a0b8ef79fbdd
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2025-02-16 00:00:57 +0800

    Setup WhisperX

--- src/handler.py
+++ src/handler.py
@@ -1,6 +1,23 @@
 import runpod
+import whisperx
+
+model = whisperx.load_model(
+    "large-v3", "cuda"
+)
 
 def run_whisperx_job(job):
-    return "this is whisperx"
+    job_input = job['input']
+    url = job_input.get('url', "")
+
+    print(f"🚧 Loading audio from {url}...")
+    audio = whisperx.load_audio(url)
+    print("✅ Audio loaded")
+
+    print("Transcribing...")
+    result = model.transcribe(audio, batch_size=16)
+    print("🎉 Transcription done:")
+    print(result["segments"])
+
+    return result["segments"]
 
 runpod.serverless.start({"handler": run_whisperx_job})
